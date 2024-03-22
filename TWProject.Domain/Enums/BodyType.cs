cc4bae662e0b61ca3c8cf13f5bc26f5793199f9b
mikhaelr12 <rusumiihai@gmail.com>
2024-03-22 17:28:58 +0200

    added some enums

---
+++ TWProject.Domain/Enums/BodyType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TWProject.Domain.Enums
+{
+	public enum BodyType
+	{
+		Van,
+		Universal,
+		Minivan,
+		SUV,
+		Cabriolet,
+		Sedan,
+		Crossover,
+		Hatchback
+	}
+}
